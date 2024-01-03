69f7fea5972f4af9ade8c7b5aff4b884f986d3ab
fabiang8 <68124971+fabiang8@users.noreply.github.com>
2024-01-02 20:56:53 -0500

    Delete leetcode/max_customerW.py

--- leetcode/max_customerW.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-
-    def maximumWealth(self, accounts: List[List[int]]) -> int:
-        wealth = []
-        for customer in accounts:
-            wealth.append(sum(customer))
-        
-        return max(wealth)
-            
